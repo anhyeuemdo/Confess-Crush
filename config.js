0cd241a6c0dbc5adfb6995124ba70e2c60d6d706
anhyeuemdo <91776319+anhyeuemdo@users.noreply.github.com>
2021-10-06 22:27:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Gửi người đó",
-    introTitle: 'là em đó',
+    introTitle: 'là chị đó, Tlinh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
