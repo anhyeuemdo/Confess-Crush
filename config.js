8030f8a8afb25ce9f74f4ff275b2ff3eceac9072
anhyeuemdo <91776319+anhyeuemdo@users.noreply.github.com>
2021-10-06 22:33:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Thế thì làm con dâu mẹ anh nha!! hihi :)))).',
+    messDesc: 'Thế thì làm con dâu mẹ anh nha!! hihi.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
